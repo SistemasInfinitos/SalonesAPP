95f9429d7d00d150d40fe96d799b930a48a4c198
bello.mauricio <bello.mauricio@WIN-DESARROLLO>
2022-03-09 13:10:00 -0500

    PersonasController

---
+++ SalonesAPI/Controllers/PersonasController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SalonesAPI.Configuration;
+using SalonesAPI.Repositorio.SalonesES;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalonesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private readonly CultureInfo culture = new CultureInfo("is-IS");
+        private readonly CultureInfo cultureFecha = new CultureInfo("en-US");
+        private readonly JwtConfig _jwtConfig;
+
+        private readonly ISalonesESRepositorio _repositorySalones;
+        public PersonasController(IOptionsMonitor<JwtConfig> optionsMonitor)
+        {
+            _jwtConfig = optionsMonitor.CurrentValue;
+            _repositorySalones = new SalonesESRepositorio(optionsMonitor);
+        }
+    }
+}
