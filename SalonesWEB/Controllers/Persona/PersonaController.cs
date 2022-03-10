dc589cdb88b3a7733758ffa18e1e433bb9d8f38f
bello.mauricio <bello.mauricio@WIN-DESARROLLO>
2022-03-10 17:09:54 -0500

    ..

--- SalonesWEB/Controllers/Persona/PersonaController.cs
+++ SalonesWEB/Controllers/Persona/PersonaController.cs
@@ -1,18 +1,34 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SalonesWEB.Controllers.Persona
 {
+    [Route("web/per")]
     public class PersonaController : Controller
     {
-        // GET: PersonaController
+        
+        [Route("[action]")]
         public ActionResult Gestion()
         {
-            return View();
+            var httpClient = new HttpClient();
+            //string url = ConfigurationManager.AppSettings.Get("Service");
+            string url = "";
+            //var json = JsonConvert.SerializeObject(Object);
+            //var data = new StringContent(json, Encoding.UTF8, "application/json");
+            //var response =  httpClient.PostAsync(url, data);
+            if (/*response.IsSuccessStatusCode*/ true)
+            {
+                //var dataObjects = response.Content.ReadAsAsync<object>().Result;
+            }
+                return View();
         }
     }
 }
