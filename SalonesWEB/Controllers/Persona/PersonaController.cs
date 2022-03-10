ab54ccad162a4345a7c896955bc754ec7b53fdca
bello.mauricio <bello.mauricio@WIN-DESARROLLO>
2022-03-10 16:46:51 -0500

    OK

---
+++ SalonesWEB/Controllers/Persona/PersonaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalonesWEB.Controllers.Persona
+{
+    public class PersonaController : Controller
+    {
+        // GET: PersonaController
+        public ActionResult Gestion()
+        {
+            return View();
+        }
+    }
+}
---
+++ SalonesWEB/Models/Persona/PersonasModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SalonesWEB.Models.Persona
+{
+    public class PersonasModel
+    {
+        public int id { get; set; }
+        [Required]
+        public string identificacion { get; set; }
+        [Required]
+        public string primerNombre { get; set; }
+        public string segundoNombre { get; set; }
+        [Required]
+        public string primerApellido { get; set; }
+        public string segundoApellido { get; set; }
+        [Required]
+        public string telefono { get; set; }
+        [Required]
+        public string correo { get; set; }
+
+        [Required]
+        public int idCiudad { get; set; }
+        [Required]
+        public int edad { get; set; }
+        public string fechaCreacion { get; set; }
+        public string fechaActualizacion { get; set; }
+        public bool estado { get; set; }
+
+        public virtual int idDepartamento { get; set; }
+    }
+}
