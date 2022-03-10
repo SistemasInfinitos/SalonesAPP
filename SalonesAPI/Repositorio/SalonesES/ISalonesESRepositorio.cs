81529e9901a36cf052af423d3717c5cc3771e451
bello.mauricio <bello.mauricio@WIN-DESARROLLO>
2022-03-10 09:44:09 -0500

    BorrarSalon

--- SalonesAPI/Repositorio/SalonesES/ISalonesESRepositorio.cs
+++ SalonesAPI/Repositorio/SalonesES/ISalonesESRepositorio.cs
@@ -12,12 +12,20 @@ namespace SalonesAPI.Repositorio.SalonesES
         /// <param name="entidad"></param>
         /// <returns></returns>
         Task<bool> ActualizarSalon(Salones entidad);
+
         /// <summary>
         /// Crea un nuevo Salon en la base de datos
         /// </summary>
         /// <param name="entidad"></param>
         /// <returns></returns>
         Task<bool> CrearSalon(Salones entidad);
+        
+        /// <summary>
+        /// Elimina una reserva o salon de la base de datos
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<bool> BorrarSalon(int id);
 
         /// <summary>
         /// trae un Salon segun si id de la base de datos
--- SalonesAPI/Repositorio/SalonesES/SalonesESRepositorio.cs
+++ SalonesAPI/Repositorio/SalonesES/SalonesESRepositorio.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using SalonesAPI.Configuration;
 using SalonesAPI.ModelsAPI;
 using SalonesAPI.ModelsAPI.DataTable;
+using SalonesAPI.ModelsDB;
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace SalonesAPI.Repositorio.SalonesES
@@ -10,14 +16,41 @@ namespace SalonesAPI.Repositorio.SalonesES
     public class SalonesESRepositorio : ISalonesESRepositorio
     {
         private readonly JwtConfiguracion _jwtConfig;
-        public SalonesESRepositorio(IOptionsMonitor<JwtConfiguracion> optionsMonitor)
+        private readonly Context _context;
+
+        public SalonesESRepositorio(IOptionsMonitor<JwtConfiguracion> optionsMonitor, Context context)
         {
             _jwtConfig = optionsMonitor.CurrentValue;
+            _context = context;
         }
+
+        private readonly CultureInfo culture = new CultureInfo("is-IS");
+        private readonly CultureInfo cultureFecha = new CultureInfo("en-US");
 
         public Task<bool> ActualizarSalon(Salones entidad)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<bool> BorrarSalon(int id)
+        {           
+            bool ok = false;
+
+            try
+            {
+                string sp = "SpDeleteReserva";
+                var date = new SqlParameter("@id", id);
+                List<SqlParameter> parametros = new List<SqlParameter>();
+                parametros.Add(new SqlParameter() { ParameterName = "@id", Value = id, SqlDbType = SqlDbType.Int });
+
+                ok = await _context.Database.ExecuteSqlRawAsync(sp, parametros)>0;
+            }
+            catch (Exception e)
+            {
+                string debug = e.Message;
+                throw;
+            }
+            return await Task.Run(() => ok);
         }
 
         public Task<bool> CrearSalon(Salones entidad)
