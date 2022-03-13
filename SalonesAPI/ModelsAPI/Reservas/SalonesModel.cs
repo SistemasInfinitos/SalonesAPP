86db64cf49c1c568f4054b5775401aa7e58e41f0
MAURICIO BELLO <bello900@hotmail.com>
2022-03-13 01:04:09 -0500

    ok

--- SalonesAPI/ModelsAPI/Reservas/SalonesModel.cs
+++ SalonesAPI/ModelsAPI/Reservas/SalonesModel.cs
@@ -2,14 +2,14 @@
 {
     public class SalonesModel
     {
-        public int id { get; set; }
+        public int? id { get; set; }
         public int idPersonaCliente { get; set; }
         public string fechaEvento { get; set; }
         public int cantidadPersona { get; set; }
         public int idMotivo { get; set; }
         public string observacion { get; set; }
         public bool estado { get; set; }
-        public string fechaCreacion { get; set; }
-        public string fechaActualizacion { get; set; }
+        public virtual string fechaCreacion { get; set; }
+        public virtual string fechaActualizacion { get; set; }
     }
 }
