7463225299459a2847c33a38e995b48116d1f2fd
alejo1912 <87353919+alejo1912@users.noreply.github.com>
2022-05-30 00:01:29 -0500

    Update Startup.cs

--- SalonesAPI/Startup.cs
+++ SalonesAPI/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using SalonesAPI.ModelsDB;
-
 namespace SalonesAPI
 {
     public class Startup
